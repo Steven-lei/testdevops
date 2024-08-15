fa1c26c0f99c7cbba1819595e47872d1b4dc8be6
zhengwen lei <aeu3000484@aspire2student.ac.nz>
2024-08-16 09:48:59 +1200

    init

---
+++ testproject.web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace testproject.web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
